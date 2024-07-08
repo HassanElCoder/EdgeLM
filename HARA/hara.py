8d68653e425507d2be1fea2f9c94bfaf30111b90
Hassan <hassanelcoder@protonmail.com>
2024-07-09 02:14:02 +0800

    Two changes: 1. Added cross_validate_linear_svm which recieves the following arguments:
    train_feature: the training data (numpy.ndarray)
    train_labels: the trainig labeles 1,2,...  (numpy.ndarray)
    param_name: the parameters that you wish to tune (default to "C")
    param_range: range for values of the parameter to tune (defaults to
    np.logspace(-2, 2, 10))
    cv: number of cross validation folds (defaults to 5)
    scoring: type of scoring metric to use in the cross validation (defaults
    to "accuracy")
    n_jobs: number of job to run concurrently.
    random_state: random seed to use in randomizing the order of the data in
    the different folds
    
    return values: two arrays with  train_scores and val_scores
    
    2. Added a test function test_read_and_validate which tests both the
    readdata() function and the by reading the HAPT Data Set and tuning a
    linear SVM on the trainig data.

--- HARA/hara.py
+++ HARA/hara.py
@@ -1,12 +1,17 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import random
 import os
-random.seed(7)
+import random
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import validation_curve
+
+
+random.seed(8)
+
+
 
 data_path="./data/HAPTDataSet/"
-
 
 
 def readdata(datapath):
@@ -17,6 +22,18 @@ def readdata(datapath):
     return X_train,y_train,X_test,y_test
 
 
+def cross_validate_linear_svm(train_feature,train_labels,param_name = "C", param_range = np.logspace(-2, 2, 10), cv = 5, scoring = "accuracy", n_jobs = 6, random_state = 8):
+    # Declare the classfier
+    clf_svc = LinearSVC(random_state = random_state)
+    train_scores, val_scores = validation_curve(clf_svc,train_feature,train_labels,param_name = param_name ,param_range = param_range ,cv = cv ,scoring = scoring ,n_jobs = n_jobs)
+    return train_scores,val_scores
+
+def test_read_and_validate():
+    X_train,y_train,X_test,y_test = readdata(data_path)
+    print(X_train.iloc[:10, :10].head())
+    print(y_train.iloc[:100,:])
+    C_params = np.logspace(-6, 3, 10)
+    cross_validate_linear_svm(X_train.values,y_train.values.flatten(),param_name = "C",param_range = C_params, cv = 5, scoring = "accuracy", n_jobs = -1, random_state = 8)
 
 
-
+test_read_and_validate()
